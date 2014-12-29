2fa2d06a9e0e3f4505251367382e5710b6a5ff60
AlmogBaku <almog.baku@gmail.com>
2014-12-29 10:47:01 +0200

    fixes travis

--- test/scripts/web-server.js
+++ test/scripts/web-server.js
@@ -9,7 +9,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname+'/..'));
+app.use(express.static(__dirname+'/../..'));
 
 var port = process.env.HTTP_PORT || 8081;
 app.listen(port, function () {
--- test/travis.conf.js
+++ test/travis.conf.js
@@ -1,12 +1,13 @@
 var _    = require('lodash');
 var conf = require("./protractor.conf.js").config;
-delete conf.seleniumAddress;
+//delete conf.seleniumAddress;
 
 exports.config = _.assign(conf, {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
   capabilities: {
-    'browserName': 'chrome',
+    'browserName': (process.env.TEST_BROWSER_NAME    || 'chrome'),
+    'version': (process.env.TEST_BROWSER_VERSION || 'ANY'),
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
     'name': 'ngFacebook Tests'
