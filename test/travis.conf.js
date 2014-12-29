00eeee1bc93b3bed71c35eceb02bdac9767d2188
AlmogBaku <almog.baku@gmail.com>
2014-12-29 10:35:47 +0200

    sauce connect?

--- test/travis.conf.js
+++ test/travis.conf.js
@@ -4,9 +4,11 @@ delete conf.seleniumAddress;
 
 exports.config = _.assign(conf, {
   capabilities: {
+    'username': process.env.SAUCE_USERNAME,
+    'accessKey': process.env.SAUCE_ACCESS_KEY,
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'ngFacebook Tests'
+    'name': 'App Tests'
   }
 });
