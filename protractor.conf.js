a9e0d1680835bd093a9c3aab0f1801e4eb3a4ff0
AlmogBaku <almog.baku@gmail.com>
2014-12-29 10:51:23 +0200

    fixes travis

---
+++ protractor.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  baseUrl: 'http://localhost:'+(process.env.HTTP_PORT || 8081)+'/test/',
+  framework: 'cucumber',
+  specs: [
+    'test/features/*.feature'
+  ],
+  cucumberOpts: {
+    require: 'test/specs/*.js',
+    format: 'summary'
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  capabilities: {
+    'browserName': (process.env.TEST_BROWSER_NAME    || 'chrome'),
+    'version': (process.env.TEST_BROWSER_VERSION || 'ANY'),
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'ngFacebook Tests'
+  }
+};
+
+if(process.env.TRAVIS_BUILD_NUMBER) {
+  delete exports.config.seleniumAddress;
+}
