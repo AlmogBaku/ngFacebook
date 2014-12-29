3b8d6f48aa7852363e44a4dfd47ed00e05fe143e
AlmogBaku <almog.baku@gmail.com>
2014-12-29 10:32:55 +0200

    sauce connect?

--- test/travis.conf.js
+++ test/travis.conf.js
@@ -3,6 +3,10 @@ var conf = require("./protractor.conf.js").config;
 delete conf.seleniumAddress;
 
 exports.config = _.assign(conf, {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY
+  capabilities: {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'ngFacebook Tests'
+  }
 });
